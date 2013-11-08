500bee88e9b8a924052c8c5e97300e03eeead90b
Eric Waters <ewaters@shutterstock.com>
2013-11-08 13:51:02 -0500

    Adding Timer and Color
     * Tests with fake timer
     * Basic color tweening
     * All tests work!

--- tests/001_basics.js
+++ tests/001_basics.js
@@ -4,7 +4,8 @@ var _ = require("lodash"),
 	Animation = require("../lib/animation");
 
 var RED = 0xff0000,
-	GREEN = 0x00ff00;
+	GREEN = 0x00ff00,
+	BLUE = 0x0000ff;
 
 var common = {
 	strands: [
@@ -38,10 +39,36 @@ var common = {
 	],
 };
 
+function all (color) {
+	var strandColors = {};
+	_.forEach(common.strands, function (strand) {
+		var colors = [];
+		for (var i = strand.start; i <= strand.end; i++) {
+			colors.push(color);
+		}
+		strandColors[ strand.id ] = colors;
+	});
+	return strandColors;
+}
+
+function installTestTimer (animation) {
+	var timer = {
+		elapsed: 0,
+	};
+	animation.newTimer = function () {
+		return {
+			elapsed: function () { return timer.elapsed; },
+		};
+	};
+	return timer;
+}
+
 function dataEqual(test, got, expected, message) {
+	if (got instanceof Error)
+		console.info(got.message);
 	var differs = JSON.stringify(got) != JSON.stringify(expected);
 	if (differs)
-		console.info(test + " difflet: " + difflet.compare(got, expected));
+		console.info("got: " + JSON.stringify(got) + ", expected: " + JSON.stringify(expected));
 	test.ok(! differs, message);
 }
 
@@ -63,7 +90,7 @@ exports.oneFrame = function (test) {
 
 	dataEqual(test,
 		animation.render(),
-		{ 1: [ RED, RED, RED, RED ] },
+		all(RED),
 		"First call to render returns all red"
 	);
 	dataEqual(test,
@@ -92,12 +119,12 @@ exports.twoFrame = function (test) {
 
 	dataEqual(test,
 		animation.render(),
-		{ 1: [ RED, RED, RED, RED ] },
+		all(RED),
 		"First call to two frame render returns all red"
 	);
 	dataEqual(test,
 		animation.render(),
-		{ 1: [ GREEN, GREEN, GREEN, GREEN ] },
+		all(GREEN),
 		"Second call to two frame render returns all green"
 	);
 	dataEqual(test,
@@ -105,6 +132,96 @@ exports.twoFrame = function (test) {
 		null,
 		"Third call to two frame render returns null"
 	);
+
+	test.done();
+};
+
+exports.timing = function (test) {
+	test.expect(5);
+
+	var payload = _.cloneDeep(common);
+	payload.animations[0].frames = [
+		{ fill: RED, time: 1 },
+		{ fill: GREEN, time: 1 },
+	];
+
+	var animation = new Animation(payload);
+	if (animation instanceof Error) {
+		console.info(animation.message);
+		return;
+	}
+	var timer = installTestTimer(animation);
+
+	dataEqual(test, animation.render(), all(RED));
+
+	timer.elapsed = 0.5;
+	dataEqual(test, animation.render(), all(RED));
+
+	timer.elapsed = 1;
+	dataEqual(test, animation.render(), all(GREEN));
+
+	timer.elapsed = 1.9;
+	dataEqual(test, animation.render(), all(GREEN));
+
+	timer.elapsed = 2;
+	dataEqual(test, animation.render(), null);
+
+	test.done();
+};
+
+exports.timingSkip = function (test) {
+	test.expect(3);
+
+	var payload = _.cloneDeep(common);
+	payload.animations[0].frames = [
+		{ fill: RED, time: 1 },
+		{ fill: GREEN, time: 1 },
+		{ fill: BLUE, time: 1 },
+	];
+
+	var animation = new Animation(payload);
+	if (animation instanceof Error) {
+		console.info(animation.message);
+		return;
+	}
+	var timer = installTestTimer(animation);
+
+	dataEqual(test, animation.render(), all(RED));
+
+	timer.elapsed = 2.1;
+	dataEqual(test, animation.render(), all(BLUE));
+
+	timer.elapsed = 4;
+	dataEqual(test, animation.render(), null);
+
+	test.done();
+};
+
+exports.transition = function (test) {
+	test.expect(3);
+
+	var payload = _.cloneDeep(common);
+	payload.animations[0].frames = [
+		{ fill: RED, time: 1 },
+		{ dissolve: true, time: 1 },
+		{ fill: BLUE, time: 1 },
+	];
+
+	var animation = new Animation(payload);
+	if (animation instanceof Error) {
+		console.info(animation.message);
+		return;
+	}
+	var timer = installTestTimer(animation);
+
+	dataEqual(test, animation.render(), all(RED));
+
+	timer.elapsed = 1.5;
+	// http://meyerweb.com/eric/tools/color-blend/
+	dataEqual(test, animation.render(), all(0x800080));
+
+	timer.elapsed = 2.1;
+	dataEqual(test, animation.render(), all(BLUE));
 
 	test.done();
 };
---
+++ tests/003_color.js
@@ -0,0 +1,31 @@
+var _ = require("lodash"),
+	Color = require("../lib/color");
+
+exports.red = function (test) {
+	var color = new Color('RGB', 0xff0000);
+	test.equal(color.red(), 1, "RGB red()");
+	test.equal(color.green(), 0, "RGB green()");
+	test.equal(color.blue(), 0, "RGB blue()");
+	test.done();
+};
+
+exports.purple = function (test) {
+	var color = new Color('RGB', 0x800080);
+	test.equal(color.red().toFixed(2) * 1, 0.5, "RGB red()");
+	test.equal(color.green(), 0, "RGB green()");
+	test.equal(color.blue().toFixed(2) * 1, 0.5, "RGB blue()");
+	test.equal(color.integer(), 0x800080, "RGB integer()");
+	test.done();
+};
+
+exports.tween = function (test) {
+	var colorA = new Color('RGB', 0xff0000),
+		colorB = new Color('RGB', 0x0000ff);
+
+	var fade = colorA.tween(0.5, colorB);
+	test.equal(fade.red().toFixed(2) * 1, 0.5, "RGB red()");
+	test.equal(fade.green(), 0, "RGB green()");
+	test.equal(fade.blue().toFixed(2) * 1, 0.5, "RGB blue()");
+	test.equal(fade.integer(), 0x800080, "RGB integer()");
+	test.done();
+};
