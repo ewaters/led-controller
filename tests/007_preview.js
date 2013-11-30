ecab4974bf76d27ce4c1fb1d9e96331f79052b49
Eric Waters <ewaters@shutterstock.com>
2013-11-30 16:34:20 -0500

    Fixed stalling test

--- tests/007_preview.js
+++ tests/007_preview.js
@@ -1,5 +1,8 @@
 var async = require("async"),
 	Playback = require("../lib/playback"),
+	spawn = require("child_process").spawn,
+	fs = require("fs"),
+	tmp = require("temporary"),
 	request = require("supertest");
 
 var RED = 0xff0000,
@@ -34,20 +37,33 @@ var baseConfig = {
 	}],
 };
 
+function binaryParser(res, callback) {
+	res.setEncoding('binary');
+	res.data = '';
+	res.on('data', function (chunk) {
+		res.data += chunk;
+	});
+	res.on('end', function () {
+		callback(null, new Buffer(res.data, 'binary'));
+	});
+}
+
 exports.basic = function (test) {
 	var playback = new Playback();
 	playback.server.listen(0);
 	var appBaseUrl = "http://127.0.0.1:" + playback.server.address().port;
 
+	var payload = {
+		config: baseConfig,
+		fps: 5,
+		layout: 1,
+	};
+
 	async.waterfall([
 		function (cb) {
 			request(appBaseUrl)
 				.post("/api/preview")
-				.send({
-					config: baseConfig,
-					fps: 5,
-					layout: 1,
-				})
+				.send(payload)
 				.expect(200)
 				.expect("Content-Type", /json/)
 				.end(function (err, res) {
@@ -61,9 +77,35 @@ exports.basic = function (test) {
 				.get(gifUri)
 				.expect(200)
 				.expect("Content-Type", "image/gif")
+				.parse(binaryParser)
 				.end(function (err, res) {
-					return cb(err);
+					if (err) return cb(err);
+					return cb(null, res);
 				});
+		},
+		function (res, cb) {
+			var identify = spawn("identify", ["-"]);
+			var frames = [];
+
+			identify.stdin.end(res.body);
+			identify.stdout.on("data", function (data) {
+				var lines = data.toString().split("\n").filter(function (l) { return l.length > 0; });
+				frames = frames.concat(lines);
+			});
+			identify.stderr.on("data", function (data) {
+				console.error("stderr data", data.toString());
+			});
+			identify.on("close", function (code) {
+				if (code != 0) {
+					console.info(frames);
+					return cb("Identify exited with non-zero status " + code);
+				}
+				test.equal(frames.length, payload.fps * payload.config.animations[0].frames.length);
+				return cb();
+			});
+		},
+		function (cb) {
+			playback.close(cb);
 		},
 	], function (err) {
 		if (err !== undefined && err !== null) {
